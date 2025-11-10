05f863d606018b6a32344ae3f398e28034f8b967
Aman <amank9thdnr@gmail.com>
2025-11-10 12:04:03 +0530

    add markview

--- nvim/lua/sample_custom.lua
+++ nvim/lua/sample_custom.lua
@@ -5,6 +5,32 @@ local M = {}
 M.plugins = {
   { "Shatur/neovim-ayu" },
 
+  {
+"OXY2DEV/markview.nvim",
+lazy = false, -- Changed from true to false as recommended
+ft = { "markdown", "typst", "latex", "yaml", "html" }, -- Added filetype loading
+-- If you use blink.cmp for completion, uncomment the next line:
+-- dependencies = { "saghen/blink.cmp" },
+config = function()
+require("markview").setup({
+preview = {
+icon_provider = "internal", -- or "mini" or "devicons"
+},
+-- Add further customization here as needed
+})
+end,
+keys = {
+{
+"<leader>mp",
+function()
+vim.cmd("Markview preview")
+end,
+desc = "Markdown Preview (Markview)",
+},
+},
+}
+
+
 }
 
 -- add extra configuration options here, like extra autocmds etc.
